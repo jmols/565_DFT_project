c9e194e22ee7dc5757695a3e809172ce80f93f6a
finneyjm <finneyjm@uw.edu>
2019-02-21 21:23:19 -0800

    Added my file for the mocalc. Still can't get it to run though

--- nucnucrepulsion.m
+++ nucnucrepulsion.m
@@ -5,11 +5,10 @@ function Vnn = nucnucrepulsion(atoms,xyz_a0)
 %   atoms: list of the elementnumbers
 %   xyz_a0: Cartesian coordinates of nuclei, in bohr
 
-Vnn=0
-for i=1:numel(atoms)
+Vnn=0;
+for i=1:numel(atoms)-1
     for j=i+1:numel(atoms)
         Vnn=Vnn+(atoms(i)*atoms(j))/(norm(xyz_a0(i,:)-xyz_a0(j,:)));
     end
 end
 end
-
--- test.m
+++ test.m
@@ -1,20 +1,22 @@
 clear, clc
-
+load('motest.mat')
 basissetdef = basisread('STO-3G');
 
-atoms = [6 8];
+atoms = [1 2];
 
-xyz_a0 = [0 0 0;0.5 0 0];
+xyz_a0 = [0 0 0;0 0 1.4632];
 
 basis = buildbasis(atoms, xyz_a0, basissetdef);
+settings.basisset = 'STO-3G';
+settings.tolEnergy = 1e-8;
+settings.tolDensity = 1e-8;
+totalcharge = 1;
 
+output = mocalc(atoms,xyz_a0,totalcharge,settings);
 
-Z = atoms(1)*basis(1).d(1)*basis(6).d(3)*basis(1).N(1)*basis(6).N(3)*...
-    aux_integral(basis(1).a,basis(6).a, basis(1).alpha(1),basis(6).alpha(3),...
-    basis(1).A, basis(6).A,xyz_a0(1,:),0);
+F = output.T + output.Vne + output.J - output.K;
+err = F*output.C-output.S*output.C*output.epsilon;
 
-Vne = int_attraction(atoms,xyz_a0,basis);
-%length(basis)
 
 %basissetdef{atoms(6)};
 
